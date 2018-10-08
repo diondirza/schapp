68b2c2fa27e8e787cf1598257579b02c1dcd72c9
Dion Dirza <dion.dirza@gmail.com>
2018-10-08 11:16:53 +0700

    Fix to minutes issue with windows

--- src/helpers/toMinutes.js
+++ src/helpers/toMinutes.js
@@ -1,9 +1,6 @@
 export default function toMinutes(data) {
-  const times = data
-    .split(':')
-    .map(Number)
-    .concat(0);
-  const ms = new Date().setHours(...times);
+  const [hours, minutes] = data.split(':').map(Number);
+  const ms = new Date().setHours(hours, minutes, 0);
   const initMs = new Date().setHours(9, 0, 0);
 
   return (ms - initMs) / 1000 / 60;
